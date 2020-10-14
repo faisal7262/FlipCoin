c08fdd6040920e917a081716417e6515922f802f
faisal7262 <shaikhfaisal6342@gmail.com>
2020-10-14 23:10:41 +0530

    using Loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -x
 
-a=$((RANDOM%2));
 
-if [[ a -eq 1 ]]
-then
-	echo "Head is winner"
-else
-	echo "Tail is winner"
-fi
+head=0;
+tail=0;
+
+#using loop uc2
+
+read -p "Enter a number for flip coin :" n
+
+for (( i=0; i<n; i++ ))
+{
+	a=$((RANDOM%2));
+
+	if [[ a -eq 1 ]]
+		then
+			head=$((head+1))
+		else
+			tail=$((tail+1))
+	fi
+}
+
+echo "number of head win" $head
+echo "number of tail win" $tail
